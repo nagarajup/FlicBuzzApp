9a8dc46d36491427004c05aaef16e1e684f3aaca
ct-nagrajpilla <PnrBdm@007>
2019-05-27 14:25:50 +0530

    deeplinking

---
+++ app/src/main/java/com/aniapps/flicbuzzapp/notifications/Notification_Act.java
@@ -0,0 +1,12 @@
+package com.aniapps.flicbuzzapp.notifications;
+
+import android.os.Bundle;
+import com.aniapps.flicbuzzapp.AppConstants;
+import org.jetbrains.annotations.Nullable;
+
+public class Notification_Act extends AppConstants {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
