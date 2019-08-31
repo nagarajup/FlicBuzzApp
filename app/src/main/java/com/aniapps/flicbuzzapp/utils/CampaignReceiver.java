af42e4acaf682715bc4175af9121dacbb4c95dba
ct-nagrajpilla <PnrBdm@007>
2019-08-31 15:43:18 +0530

    added firebase refferer api & events

--- app/src/main/java/com/aniapps/flicbuzzapp/utils/CampaignReceiver.java
+++ app/src/main/java/com/aniapps/flicbuzzapp/utils/CampaignReceiver.java
@@ -3,8 +3,18 @@ package com.aniapps.flicbuzzapp.utils;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
+import androidx.annotation.RequiresApi;
+import com.aniapps.flicbuzzapp.AppConstants;
+import com.aniapps.flicbuzzapp.activities.IntroductionScreen;
+import com.aniapps.flicbuzzapp.networkcall.APIResponse;
+import com.aniapps.flicbuzzapp.networkcall.RetrofitClient;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -15,7 +25,7 @@ public class CampaignReceiver extends BroadcastReceiver {
 
 
     @Override
-    public void onReceive(Context context, final Intent intentx) {
+    public void onReceive(final Context context, final Intent intentx) {
       //  LOGGER.info(MARKER, "on Receive called");
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.execute(new Runnable() {
@@ -25,6 +35,7 @@ public class CampaignReceiver extends BroadcastReceiver {
                     for (String key : intentx.getExtras().keySet()) {
                         try {
                             Log.e("LINK", key + " => " + String.valueOf(intentx.getExtras().get(key)));
+                            TrackReferral(context,String.valueOf(intentx.getExtras().get(key)));
                         } catch (Exception e) {
                             Log.e("LINK", "caught exception in on key retrieval ", e);
                         }
@@ -35,5 +46,37 @@ public class CampaignReceiver extends BroadcastReceiver {
             }
         });
         executorService.shutdown();
+    }
+
+    public void TrackReferral(final Context context, final String referrer) {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("action", "track_referral");
+        params.put("referral_from","Google_Tracker");
+        params.put("referral_code", referrer);
+        params.put("language", PrefManager.getIn().getLanguage().toLowerCase());
+        RetrofitClient.getInstance().doBackProcess(context, params, "online", new APIResponse() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onSuccess(String result) {
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    int status = jsonObject.getInt("status");
+                    Log.e("RES", "Referrer" + result);
+                    FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(context);
+                    mFirebaseAnalytics.setUserProperty("user_id", PrefManager.getIn().getMobile());
+                    mFirebaseAnalytics.setUserId(PrefManager.getIn().getMobile());
+                    Bundle bundle = new Bundle();
+                    bundle.putString("Category", "CampaignReceiver");
+                    bundle.putString("Action", ""+referrer);
+                    mFirebaseAnalytics.logEvent("my_events", bundle);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(String res) {
+            }
+        });
     }
 }
